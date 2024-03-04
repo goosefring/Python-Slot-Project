12092a604e89ea5dc161e0d773e38479e752749c
goosefring <bhattirehan.rb@gmail.com>
2024-03-03 23:26:37 -0700

    Create main.py
    Testing out to see if my python works in vs code.

---
+++ main.py
@@ -0,0 +1,7 @@
+#Initial function
+test = input("Enter your name: ")
+print("Welcome " + test + "!")
+# def deposit():
+#     while True:
+#         amount = input("How much $ would you like to deposit?")
+#     return
