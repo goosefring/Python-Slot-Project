b4fdb6f62aa640bfdde9a643ff42f6dbbddf2197
goosefring <bhattirehan.rb@gmail.com>
2024-03-17 18:53:14 -0600

    more work on slot machine logic

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ MAX_BET = 100
 ROWS = 3
 COLS = 3
 
+#dictionary
 symbol_count = {
     "A": 2,
     "B": 4,
@@ -16,31 +17,66 @@ symbol_count = {
     "D": 8
 }
 
+# [A] [A] [A]
+# [A] [A] [A]
+# [B] [B] [B]
+# [B] [B] [B]
+# [B] [B] [B]
+# [B] [B] [B]
+# [C] [] [A]
+# [A] [A] [A]
+# [A] [A] [A]
+# [A] [A] [A]
+# [A] [A] [A]
+# [A] [A] [A]
+# [A] [A] [A]
+# [A] [A] [A]
+
 def get_slot_machine_spin(rows, cols, symbols):
     
+    #list
     all_symbols = []
     
     #symbol = A, symbol_count = 2
+    # items() give you both key and value associated with a dictionary.
     for symbol, symbol_count in symbols.items():
         
         # underscore is used since we don't care about the count
         for _ in range(symbol_count):
+            
+            #add symbol based on what value it's been assigned in the dictionary.
             all_symbols.append(symbol)
             
     #columns = [[], [], []]
     
     columns = []
     
+    #for each col, we want to add a row. for each row we want to choose a random symbol.
     for col in range(cols):
-        column = []
         
-        #copy list
+        col = []
+        # ":" copies list since we want to remove values and not re-use the same list.
         current_symbol = all_symbols[:]
         
-        for row in range(rows):
-            value = random.choice(current_symbol)
-            current_symbol.remove(value)
+        for _ in range(rows):
             
+            #choose random symbol
+            value = random.choice(current_symbol)
+            #remove the chosen symbol from the list
+            current_symbol.remove(value)
+            col.append(value)
+            
+        columns.append(col)    
+        
+    return columns
+
+def print_slot_machine(columns):
+    
+    #transposing
+    for row in range(len(columns[0])):
+        #enumerate gives you both the index and item
+        for i, col in enumerate(columns):
+            print(col[row],)  
 
 #grab how much user would like to deposit
 def get_deposit():
